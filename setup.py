c7921c3ee76800f43e34186956122ad77e4e8583
new-user888 <146866673+new-user888@users.noreply.github.com>
2025-04-17 23:35:25 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="python-sample-app",
-    version="1.0",
+    version="1.1",
     description="A starter template for new Python applications",
     long_description=long_description,
     long_description_content_type="text/markdown",
